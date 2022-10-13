822feeae4c750b7af2576120d612231711979850
Mikhail Lavrov <lavrov.fd@gmail.com>
2022-10-13 13:13:22 +0300

    put props in order, add js for toggle menu btn

---
+++ source/js/script.js
@@ -0,0 +1,15 @@
+let navMain = document.querySelector('.header__navigation');
+let navToggle = document.querySelector('.header__navigation-button');
+let mainBody = document.querySelector('.body');
+
+mainBody.classList.remove('body--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('header__navigation--closed')) {
+    navMain.classList.remove('header__navigation--closed');
+    navMain.classList.add('header__navigation--opened');
+  } else {
+    navMain.classList.add('header__navigation--closed');
+    navMain.classList.remove('header__navigation--opened');
+  }
+});
