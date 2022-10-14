5fb2cfd5e47c91586bc17cac626a3fa2b6c32f4f
Mikhail Lavrov <lavrov.fd@gmail.com>
2022-10-14 22:29:38 +0300

    swiper settings

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -1,16 +1,17 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
 
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
   loop: true,
-  autoplay: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
+  autoplay: false,
+  keyboard: {
+    enabled: true
   },
 
-  // Navigation arrows
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
