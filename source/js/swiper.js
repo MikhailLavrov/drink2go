893356a813edcb84b94b2d402090e0e2bf335210
Mikhail Lavrov <lavrov.fd@gmail.com>
2022-10-15 16:14:52 +0300

    fixed swiper autoheight

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -2,7 +2,12 @@ import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.brow
 
 const swiper = new Swiper('.swiper', {
   loop: true,
-  autoplay: false,
+  autoplay: {
+    delay: 5500,
+  },
+  speed: 2500,
+  autoHeight: true,
+
   keyboard: {
     enabled: true
   },
